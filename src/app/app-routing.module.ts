b5c18567b52ca189a24236a8af152c216ff35a16
Uzias Rivera <uziasrivera@gmail.com>
2020-03-27 18:39:50 -0700

    added services and route to posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
+import { BodyComponent } from './components/body/body.component'
+import {CasesComponent} from './components/cases/cases.component'
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BodyComponent
+  },
+  {
+    path: 'posts',
+    component: CasesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { BodyComponent } from './components/body/body.component';
+import { CasesComponent } from './components/cases/cases.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    BodyComponent
+    BodyComponent,
+    CasesComponent
   ],
   imports: [
     BrowserModule,
